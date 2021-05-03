d1a4cd8d706d0b36a35853372226ce551978e8a7
jurusso <justin.russo98@gmail.com>
2021-05-02 21:38:35 -0400

    Remove extra import

--- topicModel.py
+++ topicModel.py
@@ -3,8 +3,6 @@ from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.stem.porter import *
-import numpy as np
-np.random.seed(2018)
 import nltk
 #nltk.download('wordnet')
 
